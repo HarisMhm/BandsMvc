62fa831384c59e6a3f1b010347c51294aefc8566
HarisM <118643704+HarisMhm@users.noreply.github.com>
2023-03-03 15:29:33 +0100

    Uppdaterat Views & BandsController

---
+++ BandsMvc/Controllers/BandsController.cs
@@ -0,0 +1,28 @@
+﻿using BandsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsMvc.Controllers
+{
+    public class BandsController : Controller
+    {
+
+        private static DataService dataService = new DataService();
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            Band[] bands = dataService.GetAll();
+            return View(bands);
+        }
+    }
+}
+
+
+
+
+//[HttpGet("")]
+//public IActionResult Index()
+//{
+//    Computer[] model = dataService.GetAll();
+//    return View(model);
+//}
